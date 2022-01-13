7d36dc0e2f5855ced87d13e0f5df8766ca127731
Benjamin <benjaminrowe0@gmail.com>
2022-01-13 21:23:02 +1100

    lab is done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', e => {
+    const pElement = document.getElementById('text');
+    pElement.textContent = "This is really cool!"
+})
